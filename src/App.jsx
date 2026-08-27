9cef5d612451747cb4baa83bd175a9fb6ba20e8a
alisheeraz007 <alisheeraz007@gmail.com>
2026-08-27 17:52:21 +0500

    added react-router-dom for routing in project and made an empty page of dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import UserRoutes from "./routes/UserRoutes";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      Started Project Ahya
-    </>
-  )
+      <UserRoutes />
+  );
 }
-
-export default App
